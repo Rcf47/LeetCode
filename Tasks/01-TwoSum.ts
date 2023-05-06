313134c7b419e7834b090fc8ce8f10b4374b118b
Vadim Cherepanov <xenos26@gmail.com>
2023-05-07 05:16:29 +0700

    feat: add solution from AI

--- Tasks/01-TwoSum.ts
+++ Tasks/01-TwoSum.ts
@@ -29,3 +29,16 @@ export function twoSum1(nums: number[], target: number): number[] {
 
   return answer
 };
+
+//solution from AI
+function twoSumAI(nums: number[], target: number): number[] {
+  const numMap = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement)!, i];
+    }
+    numMap.set(nums[i], i);
+  }
+  return [];
+}
