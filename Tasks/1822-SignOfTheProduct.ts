6fadb723208d98de7324da0043a9475d0a4ceff4
Vadim Cherepanov <xenos26@gmail.com>
2023-05-10 03:50:18 +0700

    feat: add solution from AI

--- Tasks/1822-SignOfTheProduct.ts
+++ Tasks/1822-SignOfTheProduct.ts
@@ -8,3 +8,22 @@ function arraySign(nums: number[]): number {
   }
   return 0
 };
+
+/* function signFunc(x: number): number {
+  if (x > 0) {
+    return 1;
+  } else if (x < 0) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+function arraySign(nums: number[]): number {
+  let product = 1;
+  for (let num of nums) {
+    product *= num;
+  }
+  return signFunc(product);
+}
+*/
