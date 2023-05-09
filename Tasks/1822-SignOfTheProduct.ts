7000387027d4b2d92271b1d8c8558929acb032c2
Vadim Cherepanov <xenos26@gmail.com>
2023-05-10 03:48:58 +0700

    feat: add solution for 1822 SignOfTheProduct

--- Tasks/1822-SignOfTheProduct.ts
+++ Tasks/1822-SignOfTheProduct.ts
@@ -1,3 +1,10 @@
 function arraySign(nums: number[]): number {
-
+  let productOfNum: number = nums.reduce((acc: number, item: number) => acc * item, 1)
+  if (productOfNum > 0) {
+    return 1
+  }
+  if (productOfNum < 0) {
+    return -1
+  }
+  return 0
 };
